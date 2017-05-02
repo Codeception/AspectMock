32e7fb53fc28f7e108728cfaeb4de7e855594c70
Bernd Hoehl <bhoehl@myfonts.com>
2017-05-02 10:34:03 +0200

    adding support for optional and referenced parameters in BUILT-IN functions

--- src/AspectMock/Intercept/FunctionInjector.php
+++ src/AspectMock/Intercept/FunctionInjector.php
@@ -3,54 +3,123 @@ namespace AspectMock\Intercept;
 
 class FunctionInjector
 {
-    protected $template = <<<EOF
+	protected $template = <<<EOF
 <?php
 namespace {{ns}};
 if (!function_exists('{{ns}}\{{func}}')) {
-function {{func}}() {
-    if ((\$__am_res = __amock_before_func('{{ns}}','{{func}}', func_get_args())) !== __AM_CONTINUE__) {
-        return \$__am_res;
+    function {{func}}() {
+        if ((\$__am_res = __amock_before_func('{{ns}}','{{func}}', func_get_args())) !== __AM_CONTINUE__) {
+            return \$__am_res;
+        }
+        return call_user_func_array('{{func}}', func_get_args());
     }
-    return call_user_func_array('{{func}}', func_get_args());
-}
 }
 EOF;
-    protected $namespace;
-    protected $function;
-    protected $fileName;
 
-    function __construct($namespace, $function)
-    {
-        $this->namespace = $namespace;
-        $this->function = $function;
-        $this->place('ns', $this->namespace);
-        $this->place('func', $this->function);
-
+	protected $templateByRefOptional = <<<EOF
+<?php
+namespace {{ns}};
+if (!function_exists('{{ns}}\{{func}}')) {
+    function {{func}}({{arguments}}) {
+         \$args = [];
+         switch(count(func_get_args())) {
+{{code}}         }
+         if ((\$__am_res = __amock_before_func('{{ns}}','{{func}}', \$args)) !== __AM_CONTINUE__) {
+             return \$__am_res;
+         }
+         return call_user_func_array('{{func}}', \$args);
     }
+}
+EOF;
 
-    public function save()
-    {
-        $this->fileName = tempnam(sys_get_temp_dir(), $this->function);
-        file_put_contents($this->fileName, $this->template);
-    }
+	protected $namespace;
 
-    public function inject()
-    {
-        require_once $this->fileName;
-    }
+	protected $function;
+	protected $fileName;
 
-    public function getFileName()
-    {
-        return $this->fileName;
-    }
+	function __construct($namespace, $function)
+	{
+		$this->namespace = $namespace;
+		$this->function = $function;
+		$this->placeOptionalAndReferenceFunction($namespace, $function);
+		$this->place('ns', $this->namespace);
+		$this->place('func', $this->function);
+	}
 
-    public function getPHP()
-    {
-        return $this->template;
-    }
+	public function getParameterDeclaration(\ReflectionParameter $parameter)
+	{
+		$text = (string)$parameter;
+		if (preg_match('@Parameter\s#[0-9]+\s\[\s<(required|optional)>(.*)(\sor NULL)(.*)\s\]@', $text, $match)) {
+			$text = $match(2).$match[4];
+		} elseif (preg_match('@Parameter\s#[0-9]+\s\[\s<(required|optional)>\s(.*)\s\]@', $text, $match)) {
+			$text = $match[2];
+		} else {
+			throw new \Exception('reflection api changed. adjust code.');
+		}
+		if ($parameter->isOptional()) {
+			$text .= "=NULL";
+		}
+		return $text;
+	}
 
-    protected function place($var, $value)
-    {
-        $this->template = str_replace("{{{$var}}}", $value, $this->template);
-    }
+	public function placeOptionalAndReferenceFunction($namespace, $function)
+	{
+		$reflect = new \ReflectionFunction($function);
+		$parameters = [];
+		$args = '';
+		$byRef = false;
+		$optionals = false;
+		$names = [];
+		foreach ($reflect->getParameters() as $parameter) {
+			$name = '$'.$parameter->getName();
+			$newname = '$p'.$parameter->getPosition();
+			$declaration = str_replace($name, $newname, $this->getParameterDeclaration($parameter));
+			$name = $newname;
+			if (!$optionals && $parameter->isOptional()) {
+				$optionals = true;
+			}
+			if ($parameter->isPassedByReference()) {
+				$name = '&'.$name;
+				$byRef = true;
+			}
+			$names[] = $name;
+			$parameters[$newname] = $declaration;
+		}
+		if ($optionals || $byRef) {
+			$this->template = $this->templateByRefOptional;
+			$this->place('arguments', join(', ', $parameters));
+			$code = '';
+			for ($i = count($parameters); $i > 0; $i--) {
+				$code .= "             case {$i}: \$args = [" . join(', ', $names) . "]; break;\n";
+				array_pop($names);
+			}
+			$this->place('code', $code);
+		}
+	}
+
+	public function save()
+	{
+		$this->fileName = tempnam(sys_get_temp_dir(), $this->function);
+		file_put_contents($this->fileName, $this->template);
+	}
+
+	public function inject()
+	{
+		require_once $this->fileName;
+	}
+
+	public function getFileName()
+	{
+		return $this->fileName;
+	}
+
+	public function getPHP()
+	{
+		return $this->template;
+	}
+
+	protected function place($var, $value)
+	{
+		$this->template = str_replace("{{{$var}}}", $value, $this->template);
+	}
 } 
